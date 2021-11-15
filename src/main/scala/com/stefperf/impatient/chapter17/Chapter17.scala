152b77196531d55f47fa44350e990cb47c8817fb
Stefano Perfetti <perfetti.stefano@gmail.com>
2021-11-15 20:03:24 +0100

    continuing chapter 17

--- src/main/scala/com/stefperf/impatient/chapter17/Chapter17.scala
+++ src/main/scala/com/stefperf/impatient/chapter17/Chapter17.scala
@@ -6,6 +6,7 @@ import scala.annotation.tailrec
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent._
 import scala.concurrent.duration._
+import scala.util.{Failure, Success, Try}
 
 
 object Chapter17 extends Chapter(17, "Futures") {
@@ -54,7 +55,7 @@ object Chapter17 extends Chapter(17, "Futures") {
 
     exercise(2, 3) {
       def doInOrder[T, U, V](internalFun: T => Future[U], externalFun: U => Future[V]): T => Future[V] =
-        input => for (intermediate <- internalFun(input); output <- externalFun(intermediate)) yield output
+        t => for (u <- internalFun(t); output <- externalFun(u)) yield output
 
       def chainFutures[T](funs: (T => Future[T])*): T => Future[T] = funs.reduceLeft((f, g) => doInOrder(f, g))
 
@@ -68,11 +69,8 @@ object Chapter17 extends Chapter(17, "Futures") {
     }
 
     exercise(4) {
-      def doTogether[T, U1, U2](f1: T => Future[U1], f2: T => Future[U2]): T => Future[(U1, U2)] = { input =>
-        val fut1 = f1(input)
-        val fut2 = f2(input)
-        for (u1 <- fut1; u2 <- fut2) yield (u1, u2)
-      }
+      def doTogether[T, U1, U2](f1: T => Future[U1], f2: T => Future[U2]): T => Future[(U1, U2)] = input =>
+        f1(input) zip f2(input)
 
       val waitDur = Duration(1.5 * waitFutMsecs, MILLISECONDS)
       println("Await.result(doTogether(delay((_: String).length), delay((_: String) * 2))(\"TEST\"), waitDur) = " +
@@ -91,25 +89,25 @@ object Chapter17 extends Chapter(17, "Futures") {
 
     exercise(6) {
       def repeat[T](action: () => T, until: T => Boolean): Future[T] = {
-        @tailrec
-        def cycle(): T = {
+
+        @tailrec def cycle(): T = {
           val res = action()
-          if (until(res)) res
-          else cycle()
+          if (until(res)) res else cycle()
         }
+
         Future[T] { cycle() }
       }
 
-      def readPassword(): String = { print("password: > "); scala.io.StdIn.readLine() }
+      def inputPassword(): String = { print("enter password: > "); scala.io.StdIn.readLine() }
 
       def passwordIsCorrect(pwd: String): Boolean = pwd == "secret"
 
-      println(s"You have found the correct password '${Await.result(repeat(readPassword, passwordIsCorrect), 2.minutes)}'.")
+      println(s"You have found the correct password '${Await.result(repeat(inputPassword, passwordIsCorrect), Duration.Inf)}'.")
     }
 
     exercise(7) {
 
-      def countPrimes(upToN: BigInt, certainty: Int = 10): Int = {
+      def countPrimes(upToN: BigInt, certainty: Int): Int = {
         val cores = Runtime.getRuntime.availableProcessors
         val (quotient, remainder) = upToN /% cores
         val intsPerCore: BigInt = if (remainder == 0) quotient else quotient + 1
@@ -128,11 +126,97 @@ object Chapter17 extends Chapter(17, "Futures") {
       }
 
       val n = 103
-      println(s"Among the first $n integers, ${countPrimes(n)} are probably primes.")
+      val certainty = 10
+      println(s"Among the first $n integers, ${countPrimes(n, certainty)} are probably primes with certainty $certainty.")
+    }
 
+    exercise(8, 9) {
+      import java.net.URL
+      import scala.io.Source
+
+      @tailrec def inputUrl(): URL = {
+        print("enter URL: > ")
+        val input = scala.io.StdIn.readLine()
+        val urlTry = Try{ new URL(input) }
+        urlTry match {
+          case Success(url) => url
+          case _ =>
+            println(s"'$input' is not a valid URL. Please try again.")
+            inputUrl()
+        }
+      }
+
+      def readWebPage(url: URL): String = {
+        try {
+          Source.fromURL(url).mkString
+        }
+        catch {
+          case ex =>  // intentionally catching all throwables
+            println(s"It was not possible to read the webpage at address '$url' due to this error:\n$ex")
+            ""
+        }
+      }
+
+      def extractAbsoluteHyperlinks(html: String): List[URL] = {
+        val absoluteLinkPattern = """<.*? href=["'](http.*?)["'].*?>""".r("href")
+        (for (anchor <- absoluteLinkPattern.findAllMatchIn(html); link = anchor.group("href").toLowerCase)
+          yield Try{ new URL(link)}.toOption
+        ).toList.flatten
+      }
+
+      def getValidAbsoluteHyperlinks(maxDuration: Duration): Option[List[URL]] = {
+        val futureUrlList = Future{ inputUrl() }.flatMap(
+          url => Future{ readWebPage(url) }).flatMap(
+          html => Future{ Option(extractAbsoluteHyperlinks(html)) }).recover{
+          case ex =>
+            println(s"It was not possible to complete the operation due to this error:\n$ex")
+            None
+        }
+        Await.result(futureUrlList, maxDuration)
+      }
+
+      def getServer(url: URL): Option[String] =
+        Try{ Option(url.openConnection().getHeaderField("Server")) }.fold[Option[String]](_ => None, os => os)
+
+      val maxSecondsOp1 = 60
+      println(s"In the next $maxSecondsOp1 seconds, we will try to read and process a URL from the user.")
+      val result = getValidAbsoluteHyperlinks(Duration(maxSecondsOp1, SECONDS))
+      result match {
+        case None =>
+        case Some(Nil) => println("No valid absolute hyperlinks were found in the given webpage.")
+        case Some(urls) =>
+          val uniqueUrls = urls.distinct
+          println("-- List of valid absolute hyperlinks found in the given webpage:")
+          for (url <- uniqueUrls.sortBy(_.toString)) {
+            println(s"- $url")
+          }
+          println("-- End of list.")
+          println()
+          val maxSecondsOp2 = 30
+          println(s"In the next $maxSecondsOp2 seconds, we will try to compile a list of all server headers at those links.")
+          val futureServers = Future.sequence(
+            uniqueUrls.map(
+              url => Future { getServer(url) }.recover{ case _ => None }
+            )
+          )
+          Await.ready(futureServers, Duration(maxSecondsOp2, SECONDS))
+          val Some(result) = futureServers.value
+          result match {
+            case Failure(exception) => println(s"The operation failed with error:\n$exception")
+            case Success(seq) =>
+              val servers = seq.flatten.distinct.sorted
+              if (servers.isEmpty)
+                println("No server headers were found.")
+                else {
+                  println("-- List of server headers found:")
+                for (server <- servers) println(s"- $server")
+                  println("-- End of list.")
+                }
+          }
+      }
     }
 
     println("WORK IN PROGRESS")
-  }
+    }
 
 }
