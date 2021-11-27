8f77ad555ffe2e60404a366670b5955d17e309ee
Stefano Perfetti <perfetti.stefano@gmail.com>
2021-11-28 00:35:03 +0100

    finished chapter 18

--- src/main/scala/com/stefperf/impatient/chapter18/Chapter18.scala
+++ src/main/scala/com/stefperf/impatient/chapter18/Chapter18.scala
@@ -88,6 +88,41 @@ object Chapter18 extends Chapter(18, "Type Parameters") {
       println("A mutable generic container cannot be covariant in its element type because that is an input type to its setter methods.")
     }
 
-    println("WORK IN PROGRESS")
+    exercise(9) {
+      val text =
+        """// luckily, this code is illegal!
+          |class Pair[+T](val first: T, val second: T) {
+          |  def replaceFirst(newFirst: T): Pair[T] = { new Pair(newFirst, second) }
+          |}
+          |
+          |class NastyDoublePair(_first: Double, _second: Double) extends Pair[Double](_first, _second) {
+          |  override def replaceFirst(newFirst: Double): Pair[Double] = new Pair[Double](Math.sqrt(newFirst), second)
+          |}
+          |
+          |val pair: Pair[Any] = new NastyDoublePair(3.14, 2.718)
+          |pair.replaceFirst(\"Hello\")  // this line would throw an IllegalArgumentException
+          |""".stripMargin
+      println(text)
+    }
+
+    exercise(10) {
+      class Pair[S, T](var s: S, var t: T) {
+        // this trick was the only way I found to let the implicit evidence for the type constraint be found
+        def swap(unusedParameterJustAsATrickToLetImplicitBeFound: S = s)(implicit ev: S =:= T): Unit = {
+          val u = s
+          s = t.asInstanceOf[S]
+          t = u.asInstanceOf[T]
+        }
+
+        override def toString: String = s"<$s, $t>"
+      }
+
+      val pair = new Pair(3.14, 2.718)
+      println(s"pair before swap = $pair")
+      pair.swap()
+      println(pair)
+      println(s"pair after swap =  $pair")
+    }
+
   }
 }
